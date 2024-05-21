a5d738f9aaf4ef1f6a62ecab6bcbebc8acd6804a
Phi Tiet <47044950+PhiTiet@users.noreply.github.com>
2024-05-21 18:32:30 +0200

    Create start_and_pull.sh

---
+++ start_and_pull.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+app_name="portfolio"
+image_name="phitiet/portfolio:latest"
+
+stop_container() {
+    docker stop $app_name >/dev/null 2>&1 || true
+}
+
+deploy_container() {
+    docker rm $app_name >/dev/null 2>&1 || true
+    docker run -d --name $app_name -p 8080:8080 $image_name
+    echo 'Application started successfully'
+}
+
+pull_output=$(docker pull --platform arm64 $image_name 2>&1)
+
+if [[ $pull_output == *"Downloaded newer image"* ]]; then
+    stop_container
+    deploy_container
+elif [[ $pull_output == *"Image is up to date"* ]]; then
+    echo "Image is up-to-date"
+    if ! docker ps | grep -q $app_name; then
+        deploy_container
+    fi
+else
+    echo "Failed to pull the latest image."
+    echo "$pull_output"
+    exit 1
+fi
